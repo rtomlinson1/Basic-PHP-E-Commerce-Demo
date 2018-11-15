2c2f44f4756e186cabcd633f2d17eb5a04bd8b49
Ryan Tomlinson <35232317+rtomlinson1@users.noreply.github.com>
2018-11-15 10:29:34 -0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
   <head>
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
     <link rel="stylesheet" href="../css/overwrites.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
